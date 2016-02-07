4920ab8f48f57666e4dc4a4ed24a1c6f0466e033
andre-lehnert <contact@andre-lehnert.de>
2016-02-07 16:33:10 +0100

    Festival TTS + npm say added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var say = require('say');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    
+  var text = 'Hello World!';  
+    
+  say.speak('voice_cmu_us_clb_arctic_clunits', text);
+    
   res.render('index', { title: 'Express' });
 });
 
