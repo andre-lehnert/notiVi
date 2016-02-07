e195fa3a34fabd9afdc69e385cae92cab33a5741
andre-lehnert <contact@andre-lehnert.de>
2016-02-07 21:09:13 +0100

    Added tts version overview

--- routes/tts.js
+++ routes/tts.js
@@ -1,4 +1,17 @@
-'use strict';
+/* ***************************
+ * Text-To-Speech Middleware
+ * by Andre Lehnert 
+ * 0.1.0: - Play tts example sound directly
+ *        - Client-side "responsive voice" examples
+ * 0.2.0: - Play text given by an URL parameter
+ *          - GET /tts/speak?text=[TEXT]
+ * 0.3.0: - Play text given by a HTTP-POST
+ *          - POST /tts/speak 
+ *            JSON-Body
+ *            { text: "" }
+ * ***************************
+ */
+var version = '0.1.0';
 
 var express = require('express');
 var router = express.Router();
@@ -15,9 +28,12 @@ router.get('/', function(req, res, next) {
 	var content =
 	{
 		title: 'Text-To-Speech',
+		subtitle: 'TTS Examples',
+		header1: 'Client-side audio examples',
+		header2: 'Server-side Festival Speech Synthesis System',
 		
 		author: 'Andre Lehnert',
-		version: '2016 - Version 0.0.1'
+		version: '2016 - Version ' + version
 	};
 	
 	var ttsText = 'Hello I am a raspberry pi with text to speech functionality.';
