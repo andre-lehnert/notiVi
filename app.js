bb5b25c2937fdbe4322f1e7ae4198fed0104ba20
Andre Lehnert <Andre Lehnert>
2016-02-07 16:40:34 +0100

    Added Bootstrap UI und TTS View

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/* ***************************
+ * ExpressJS
+ * ***************************
+ */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,27 +9,52 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
+
+
+
+/* ***************************
+ * View-HTML-Rendering-Engine
+ * ./views/*
+ * ***************************
+ */
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+/* ***************************
+ * Utilities
+ * ***************************
+ */
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+/* ***************************
+ * Routing Scripts 
+ * ./routes/*
+ * ***************************
+ */
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var tts = require('./routes/tts');
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/tts', tts);
 
+
+/* ***************************
+ * Error Handling
+ * ***************************
+ */
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/tts.js
+++ routes/tts.js
@@ -1,18 +1,28 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* ***************************
+ * TTS
+ * ***************************
+ */
+
+
+/* GET tts page. */
 router.get('/', function(req, res, next) {
 	
 	var content =
 	{
-		title: 'Express',
+		title: 'Text-To-Speech',
 		
 		author: 'Andre Lehnert',
 		version: '2016 - Version 0.0.1'
 	};
 	
-	res.render('index', content);
+	
+	
+	res.render('tts', content);
 });
 
 module.exports = router;
